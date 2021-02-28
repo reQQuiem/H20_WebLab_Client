896f8f4d408758bbce125661c52ec0f23213c1c5
Mischa Kälin <mischa.kaelin@stud.hslu.ch>
2021-02-28 22:56:12 +0100

    Add 404 - page not found!

--- travelblog-client/src/app/app-routing.module.ts
+++ travelblog-client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MyBlogsComponent } from './my-blogs/my-blogs.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { TravelblogDetailComponent } from './travelblog-detail/travelblog-detail.component';
 import { TravelblogsComponent } from './travelblogs/travelblogs.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: 'travelblogs', component: TravelblogsComponent },
   { path: 'travelblog/:id', component: TravelblogDetailComponent },
   { path: 'my-blogs', component:  MyBlogsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PagenotfoundComponent }
 ];
 
 @NgModule({
